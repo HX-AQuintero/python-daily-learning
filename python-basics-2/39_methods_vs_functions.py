1bf056183144bbafc2bb2215f28c1f66ce1c7e68
HX-AQuintero <alejandroquinteromejia@gmail.com>
2025-02-08 16:28:33 -0500

    39

---
+++ python-basics-2/39_methods_vs_functions.py
@@ -0,0 +1,17 @@
+# Methods vs Functions
+
+# Built-in Functions
+list()
+print()
+max()
+min()
+input()
+
+# Custom Functions
+def some_random_stuff():
+  pass
+
+# Methods come with dot notation
+'hello'.capitalize()
+[4., 5., 7.].count(5.)
+{'a': 1, 'b': 2, 'c': 3}.keys()
