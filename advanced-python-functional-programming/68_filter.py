0bd4d3fbc99d41c880c9d82fd5230d39abff117c
HX-AQuintero <alejandroquinteromejia@gmail.com>
2025-03-10 21:48:13 -0500

    68

---
+++ advanced-python-functional-programming/68_filter.py
@@ -0,0 +1,41 @@
+# Filter
+"""
+The filter function is a built-in function that is used to filiter elements from an iterabole based on a condition.
+It applies a function that returns True or False to each element and keeps only those that return True.
+"""
+
+numbers = [1,2,3,4,5,6]
+def is_even(number):
+  return number % 2 == 0
+
+filtered_numbers = list(filter(is_even, numbers))
+print(filtered_numbers)
+print(numbers)
+
+names = ("Alice", "Bob", "Anna", "Charlie", "Alex")
+def starts_with_a(name):
+  return name[0].upper() == "A"
+
+filtered_names = tuple(filter(starts_with_a, names))
+print(filtered_names)
+print(names)
+
+# Filter with sets
+words = {"hello", " ", "", "world", "  ", "Python"}
+def is_not_empty(text):
+  return bool(text.strip())
+
+filtered_words = set(filter(is_not_empty, words))
+print(filtered_words)
+print(words)
+
+# Filter with dictionaries
+grades = {"Alice": 45, "Bob": 78, "Charlie": 92, "David": 30}
+def is_above_50(item):
+  key, value = item
+  return value > 50
+
+filtered_grades = dict(filter(is_above_50, grades.items()))
+
+print(filtered_grades)
+print(grades)
---
+++ advanced-python-functional-programming/68_map.py
@@ -0,0 +1,41 @@
+# Map()
+"""
+The map function is a built-in function that applies a given function to each item in an iterable
+(list, tuple, etc.), returning a new iterable without modifying the original one.
+"""
+my_list = [1,2,3]
+def multiply_by2(item):
+  return item*2
+
+my_new_list = list(map(multiply_by2, my_list))
+print(my_new_list)
+print(my_list)
+
+words = ['hello', 'world', 'my', 'friends']
+resulting_words = list(map(str.capitalize, words))
+print(resulting_words)
+print(words)
+
+# Map with tuples
+my_tuple = (1,2,3,4,5,6)
+tuple_doubled = tuple(map(multiply_by2, my_tuple))
+print(tuple_doubled)
+print(my_tuple)
+
+# Map with sets
+my_set = {1,2,3,4,5,6,7,8}
+set_doubled = set(map(multiply_by2, my_set))
+print(set_doubled)
+print(my_set)
+
+# Map with dictionaries
+data = {1: 'apple', 2: 'banana', 3: 'cherry', 4: 'pear'}
+
+def transform_key_value(item):
+  key, value = item
+  return (key*2, value.capitalize())
+
+transformed_data = dict(map(transform_key_value, data.items()))
+print(transformed_data)
+print(data)
+print(data.items())
