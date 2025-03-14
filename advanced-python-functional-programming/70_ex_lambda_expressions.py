2a74fae61daa9ca65930742fa817ffd3f7a50e07
HX-AQuintero <alejandroquinteromejia@gmail.com>
2025-03-13 21:55:38 -0500

    71

--- advanced-python-functional-programming/70_ex_lambda_expressions.py
+++ advanced-python-functional-programming/70_ex_lambda_expressions.py
@@ -5,17 +5,18 @@ my_list = [5,4,3]
 print(list(map(lambda item: item**2, my_list)))
 
 # List Sorting
-a = [(0,2), (4,3), (9,9), (10, -1)]
+list = [(0,2), (4,3), (9,9), (10, -1)]
 
 def sorter(a):
-  b = []
   for i in range(len(a)):
-    for j in range(len(a)-1):
-      initial_value = a[0]
-      if a[i][1] <= a[j+1][1]:
-        a[i] = a[j+1]
-        initial_value = a[i]
-      b.append(initial_value)
-  return b
+    for j in range(len(a)-i-1):
+      if a[j][1] >= a[j+1][1]:
+        a[j], a[j+1] = a[j+1], a[j]
+  return a
 
-print(sorter(a))
+print(sorter(list))
+
+# A better solution using lambda expressions
+list = [(0,2), (4,3), (9,9), (10, -1)]
+list.sort(key=lambda x: x[1])
+print(list)
